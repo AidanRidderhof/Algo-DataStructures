bffe56e1fe9a831d24fcb68195b3d091269ce9d7
Aidan Ridderhof <aidan.ridderhof@gmail.com>
2025-07-20 18:54:36 -0700

    remove if statement from bubble py

--- sorting/bubbleSort.py
+++ sorting/bubbleSort.py
@@ -3,12 +3,12 @@ def bubbleSort(array):
     while swap:
         swap = False
         for items in range(len(array)-1):
-            if array[items+1]:
-                if array[items+1] < array[items]:
-                    swap = True
-                    array[items], array[items+1] = array[items+1], array[items]
+            if array[items+1] < array[items]:
+                swap = True
+                array[items], array[items+1] = array[items+1], array[items]
     return array
             
 arrs = [6,4, 8, 9, 2, 5]
 print(arrs)
 print(bubbleSort(arrs))
+print(bubbleSort([99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]))
